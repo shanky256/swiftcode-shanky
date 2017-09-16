49f5667d9c1149b4b32e845f5efb0ea3eb48497a
Live session user <shashankr256@gmail.com>
2017-09-16 13:15:37 +0000

    with login

---
+++ app/data/LoginForm.java
@@ -0,0 +1,14 @@
+package data;
+
+public class LoginForm
+{
+	public String username, password;
+	public String getUsername() {
+       return username;
+   }
+
+   public String getPassword() {
+       return password;
+   }
+
+}
